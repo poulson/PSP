8589514026cb1fa5b516fe36169ab54325d24fff
Jack Poulson <jack.poulson@gmail.com>
2011-02-01 23:02:12 -0600

    The test executables can now be built on Shark.

--- build_on_shark.sh
+++ build_on_shark.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx.openmpi \
-      -DCMAKE_C_COMPILER=/usr/bin/mpicc.openmpi \
-      -DCMAKE_Fortran_COMPILER=/usr/bin/mpif90.openmpi \
+cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx.mpich \
+      -DCMAKE_C_COMPILER=/usr/bin/mpicc.mpich \
+      -DCMAKE_Fortran_COMPILER=/usr/bin/mpif90.mpich \
+      -DFortran_FLAGS="-O3 -I/usr/lib/mpich/include" \
       -DINTEL_ROOT=/opt/intel/Compiler/11.1/072 ..
 make
 
