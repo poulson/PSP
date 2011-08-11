22511b4c7bd7460cd0e1a9cd8e285cd9c5158831
Jack Poulson <jack.poulson@gmail.com>
2011-08-10 21:49:51 -0500

    Updating toolchain files.
    --HG--
    rename : cmake/toolchains/BGP-xl_dec2010-essl_4.3.1-1.cmake => cmake/toolchains/BGP-xl_apr2011-essl_4.3.1-1.cmake

--- cmake/toolchains/Lonestar-intel_11.1-mvapich2_1.6-mkl_10.3.cmake
+++ cmake/toolchains/Lonestar-intel_11.1-mvapich2_1.6-mkl_10.3.cmake
@@ -1,6 +1,11 @@
-# We need MPI C and CXX compilers, but only a serial Fortran compiler
-set(CMAKE_C_COMPILER /opt/apps/intel11_1/mvapich2/1.6/bin/mpicc)
-set(CMAKE_CXX_COMPILER /opt/apps/intel11_1/mvapich2/1.6/bin/mpicxx)
+# The serial Intel compilers
+set(CMAKE_C_COMPILER       /opt/apps/intel/11.1/bin/intel64/icc)
+set(CMAKE_CXX_COMPILER     /opt/apps/intel/11.1/bin/intel64/icpc)
+set(CMAKE_Fortran_COMPILER /opt/apps/intel/11.1/bin/intel64/ifort)
+
+# The MPI wrappers for the C and C++ compilers
+set(MPI_C_COMPILER   /opt/apps/intel11_1/mvapich2/1.6/bin/mpicc)
+set(MPI_CXX_COMPILER /opt/apps/intel11_1/mvapich2/1.6/bin/mpicxx)
 
 set(MATH_LIBS "-L/opt/apps/intel/11.1/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_sequential -lmkl_core /opt/apps/intel/11.1/lib/intel64/libifcore.a /opt/apps/intel/11.1/lib/intel64/libsvml.a -lm")
 
