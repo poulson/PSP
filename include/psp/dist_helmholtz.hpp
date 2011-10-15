4b5a53a1868b6f03267f127bb498d248ea476713
Jack Poulson <jack.poulson@gmail.com>
2011-10-15 15:53:19 -0500

    Adding a bit more towards the infrastructure of the preconditioner.

---
+++ include/psp/dist_helmholtz.hpp
@@ -0,0 +1,109 @@
+/*
+   Parallel Sweeping Preconditioner (PSP): a distributed-memory implementation
+   of a sweeping preconditioner for 3d Helmholtz equations.
+
+   Copyright (C) 2011 Jack Poulson, Lexing Ying, and
+   The University of Texas at Austin
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef PSP_DIST_HELMHOLTZ_HPP
+#define PSP_DIST_HELMHOLTZ_HPP 1
+
+#include "clique.hpp"
+
+namespace psp {
+
+template<typename F>
+class DistHelmholtz
+{
+private:
+    typedef elemental::RealBase<F>::type R;
+
+    elemental::mpi::Comm comm_;
+    const FiniteDiffControl control_;
+
+    // Useful constants
+    const R hx_, hy_, hz_; // grid spacings
+    const R bx_, by_, bz_; // (PML width)/(grid spacings)
+    const int bzPadded_;   // ceil(bz)
+
+    // Sparse matrix storage
+    int localHeight_;
+    std::vector<std::vector<F> > localRows_;
+
+    // Sparse matrix communication information
+    int allToAllSize_;
+    std::vector<int> localSendIndices_; // length is p*allToAllSize_
+    std::vector<int> localRecvIndices_; // length is p*allToAllSize_
+
+    // TODO: 
+    // Information for communication needed by application of A_{i+1,i} blocks.
+    // This will be needed for more general stencils than SEVEN_POINT.
+
+    // Sparse-direct symbolic factorizations of PML-padded panels.
+    // Since most of the inner panels are structurally equivalent, we can get
+    // away with only a few symbolic factorizations.
+    clique::symbolic::SymmFact 
+        mainSymbolicFact_, misfitSymbolicFact_, lastSymbolicFact_;
+
+    // Sparse-direct numeric factorizations of PML-padded panels
+    std::vector<clique::numeric::SymmFrontTree<F>*> numericFacts_;
+
+public:
+    DistHelmholtz
+    ( const FiniteDiffControl<F>& control, elemental::mpi::Comm comm );
+
+    ~DistHelmholtz();
+
+    // Build the sparse matrix and the preconditioner
+    void Initialize( const F* localSlowness );
+
+    // Destroy the sparse matrix and the preconditioner
+    void Finalize();
+
+    // Y := alpha A X + beta Y
+    void Multiply( F alpha, const F* localX, F beta, F* localY ) const;
+
+    // Y := approximateInv(A) Y
+    void Precondition( F* localY ) const;
+
+    // Return the number of rows of the sparse matrix that our process stores.
+    int LocalSize() const;
+
+    //void WriteParallelVtkFile( const F* vLocal, const char* basename ) const;
+};
+
+//----------------------------------------------------------------------------//
+// Implementation begins here                                                 //
+//----------------------------------------------------------------------------//
+
+template<typename F>
+inline void
+DistHelmholtz<F>::~DistHelmholtz() 
+{
+    const int numPanels = numericFacts_.size();
+    for( int i=0; i<numPanels; ++i )
+        delete numericFacts_[i];
+    numericFacts_.clear();
+}
+
+template<typename F>
+inline int
+DistHelmholtz<F>::LocalSize() const
+{ return localHeight_; }
+
+} // namespace psp
+
+#endif // PSP_DIST_HELMHOLTZ_HPP
---
+++ include/psp/finite_diff_control.hpp
@@ -0,0 +1,72 @@
+/*
+   Parallel Sweeping Preconditioner (PSP): a distributed-memory implementation
+   of a sweeping preconditioner for 3d Helmholtz equations.
+
+   Copyright (C) 2011 Jack Poulson, Lexing Ying, and
+   The University of Texas at Austin
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef PSP_FINITE_DIFF_CONTROL_HPP
+#define PSP_FINITE_DIFF_CONTROL_HPP 1
+
+namespace psp {
+
+enum BoundaryCondition { PML, DIRICHLET };
+enum Stencil { SEVEN_POINT }; // TWENTY_SEVEN_POINT not yet supported
+
+// Our control structure for defining the basic parameters for the problem 
+// to be solved. The domain is assumed to be of the form:
+//
+//                 _______________ (wx,wy,0)
+//                /              /|
+//            x  /              / |
+//              /              /  |
+// sweep dir.  /______________/   |
+//     ||      |              |   |
+//     ||      |              |   / (wx,wy,wz)
+//     ||    z |              |  /  
+//     ||      |              | /  
+//     \/      |______________|/
+//          (0,0,wz)    y    (0,wy,wz)
+//
+// PML must be enforced at least on the top face, and the remaining faces 
+// may be set as either PML or a zero Dirichlet boundary condition.
+template<typename F>
+struct FiniteDiffControl
+{
+    typedef elemental::RealBase<F>::type R;
+
+    Stencil stencil; // only 7-point supported so far
+    int nx, ny, nz;  // number of grid points in each direction
+    R wx, wy, wz;    // width of the PML-padded box in each direction
+    R omega;         // frequency of problem
+
+    R Cx, Cy, Cz; // coefficient for PML in each direction
+    R etax, etay, etaz; // widths of PML in each direction
+
+    R imagShift; // stabilizing shift for preconditioner
+    int planesPerPanel; // number of xy planes to put into each panel
+
+    BoundaryCondition frontBC;
+    BoundaryCondition rightBC;
+    BoundaryCondition backBC;
+    BoundaryCondition leftBC;
+    BoundaryCondition bottomBC;
+    // The top boundary condition must be PML since we are sweeping from it.
+};
+
+} // namespace psp
+
+#endif // PSP_FINITE_DIFF_CONTROL_HPP
