89b974da3942435d2621de515f5cfe6107eeda00
Jack Poulson <jack.poulson@gmail.com>
2012-12-21 11:53:53 -0600

    Shortening copyright notices

--- include/psp/dist_spectral_helmholtz.hpp
+++ include/psp/dist_spectral_helmholtz.hpp
@@ -1,22 +1,10 @@
 /*
-   Parallel Sweeping Preconditioner (PSP): a distributed-memory implementation
-   of a sweeping preconditioner for 3d Helmholtz equations.
-
-   Copyright (C) 2011-2012 Jack Poulson, Lexing Ying, and
+   Copyright (C) 2011-2012 Jack Poulson, Lexing Ying, and 
    The University of Texas at Austin
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+   This file is part of Parallel Sweeping Preconditioner (PSP) and is under the
+   GNU General Public License, which can be found in the LICENSE file in the 
+   root directory, or at <http://www.gnu.org/licenses/>.
 */
 #ifndef PSP_DIST_SPECTRAL_HELMHOLTZ_HPP
 #define PSP_DIST_SPECTRAL_HELMHOLTZ_HPP 1
